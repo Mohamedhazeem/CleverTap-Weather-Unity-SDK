0d82c76ebea4db7d6eeaf01a73f1099de7d417fb
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2025-12-07 10:59:17 +0530

    update toast gravity android

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,18 @@ All notable changes to this project will be documented in this file.
 
 ---
 
+## [1.0.2] - 2025-12-07
+
+### Changed
+
+- Updated Android Toast gravity to Top-Center behavior.
+
+## [1.0.1] - 2025-12-07
+
+### Added
+
+- Added a public helper method to show any custom toast message.
+
 ## [1.0.0] - 2025-12-06
 
 ### Added
@@ -18,9 +30,3 @@ All notable changes to this project will be documented in this file.
 - Unit test examples
 - Example Weather App usage
 - Full README documentation
-
-## [1.0.1] - 2025-12-07
-
-### Added
-
-- Show any toast message function
--- Runtime/Toast/ToastNativeBridge.cs
+++ Runtime/Toast/ToastNativeBridge.cs
@@ -7,20 +7,29 @@ namespace CleverTap.WeatherSDK.ToastSystem
 #if UNITY_ANDROID
         public static void ShowAndroidToast(string message)
         {
-            using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
-            {
-                AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+           using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+    {
+        AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
 
-                activity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
-                {
-                    using (AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast"))
-                    {
-                        AndroidJavaObject toastObj =
-                            toastClass.CallStatic<AndroidJavaObject>("makeText", activity, message, 0);
-                        toastObj.Call("show");
-                    }
-                }));
+        activity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+        {
+            using (AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast"))
+            using (AndroidJavaClass gravityClass = new AndroidJavaClass("android.view.Gravity"))
+            {
+                AndroidJavaObject toast = toastClass.CallStatic<AndroidJavaObject>(
+                    "makeText", activity, message, 0
+                );
+
+                int gravity = gravityClass.GetStatic<int>("TOP") |
+                              gravityClass.GetStatic<int>("CENTER_HORIZONTAL");
+
+                toast.Call("setGravity", gravity, 0, 150);  
+                // last param is Y offset (move slightly down from top)
+
+                toast.Call("show");
             }
+        }));
+    }
         }
 #endif
 
